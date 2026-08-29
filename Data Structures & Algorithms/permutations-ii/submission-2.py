325f100909ed1ddd02c552ffeba7c6ea5124d44f
Aakash Pathak <npathak76@gmail.com>
2026-08-29 15:45:10 +1000

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res, n = [], len(nums)
+        visited = [False]*n
+        temp = []
+
+        def dfs():
+            if len(temp)==n:
+                res.append(temp.copy())
+                return
+            for i in range(n):
+                if visited[i]:
+                    continue
+                if i and nums[i]==nums[i-1] and not visited[i-1]:
+                    continue
+                temp.append(nums[i])
+                visited[i]=True
+                dfs()
+                visited[i]=False
+                temp.pop()
+
+        dfs()
+        return res
