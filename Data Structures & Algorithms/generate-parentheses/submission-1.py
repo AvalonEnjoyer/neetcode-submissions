c3c99a68daf876da4d496fb2f2f1538dfd887382
Aakash Pathak <npathak76@gmail.com>
2026-08-30 02:24:08 +1000

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = [[] for _ in range(n+1)]
+        res[0] = [""]
+
+        for k in range(1,n+1):
+            for i in range(k):
+                for left in res[i]:
+                    for right in res[k-i-1]:
+                        res[k].append("("+left+")"+right)
+        print(res)
+        return res[-1]
